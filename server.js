ef64a2fd6bbd7ce6d97e3ae530a7d4b30dcde187
creep <creep.wei@gmail.com>
2016-10-09 10:16:14 +0800

    Squashed commit of the following:
    commit 2da1bbff8abdf66ab15ac88b0ba50aef7c610346
    Author: creep <creep.wei@gmail.com>
    Date:   Fri Sep 30 17:55:25 2016 +0800
    
        add res header:server
    
    commit 15ceaeda57c650c2d8079d9bc43cc2d7181a5d03
    Author: creep <creep.wei@gmail.com>
    Date:   Fri Sep 30 16:33:09 2016 +0800
    
        update key cer name
    
    commit 5656314a92f0d79b3c9409328ebad82d663dc319
    Author: creep <creep.wei@gmail.com>
    Date:   Fri Sep 30 16:27:17 2016 +0800
    
        rename app.js -> server.js
    
    commit 2500001646ace8b1cc33213b4d393d04a10e83a6
    Author: creep <creep.wei@gmail.com>
    Date:   Fri Sep 30 16:20:00 2016 +0800
    
        add gitignore rules
    
    commit 3738e3bf91a4570a4fc2e4a514fc5f26b0c6e2c1
    Author: creep <creep.wei@gmail.com>
    Date:   Fri Sep 30 12:38:00 2016 +0800
    
        bug fix & 代码整理
    
    commit 065eda2a7d1edbe5a34ba2878bf1a1956ea515ee
    Author: creep <creep.wei@gmail.com>
    Date:   Fri Sep 30 11:35:23 2016 +0800
    
        add ecstatic
    
    commit 0c380616df056cd7fdf81eecaabf896d5c6ac9f5
    Author: creep <creep.wei@gmail.com>
    Date:   Thu Sep 29 10:41:48 2016 +0800
    
        bug fix
    
    commit 31b2059257599223b719c1424cd236fba240ffb3
    Author: creep <creep.wei@gmail.com>
    Date:   Wed Sep 28 15:15:45 2016 +0800
    
        rename sslkey path
    
    commit 81da38e5e9e69acc7b2f2bb86bde6b803efa43ec
    Author: creep <creep.wei@gmail.com>
    Date:   Wed Sep 28 14:39:11 2016 +0800
    
        remove sudo
    
        remove keys -> sslkey
    
    commit 28360223abd2f37736f1cbf41174db17cbe4962f
    Author: creep <creep.wei@gmail.com>
    Date:   Wed Sep 28 14:14:49 2016 +0800
    
        add https support
    
    commit d627b45713e0b69ea0182244acb914be9662dc92
    Author: creep <creep.wei@gmail.com>
    Date:   Wed Sep 28 10:32:27 2016 +0800
    
        modify fn name
    
    commit eb5a67cb27989e4fd97e9a954e16afd96485d0c5
    Author: creep <creep.wei@gmail.com>
    Date:   Tue Sep 27 19:41:58 2016 +0800
    
        modify .gitignore
    
    commit f9f7eacaf032307f1a0b3c84a76575f3e19e5f24
    Author: creep <creep.wei@gmail.com>
    Date:   Tue Sep 27 19:29:07 2016 +0800
    
        stage-1
    
    commit aecfbd6e625b649489dd5357ce8cd3c7981bc30d
    Author: creep <creep.wei@gmail.com>
    Date:   Tue Sep 27 14:41:38 2016 +0800
    
        add som dependencies and develope script
    
    commit 592fae9588a758e6f3b56daed51a96560f4e2690
    Author: creep <creep.wei@gmail.com>
    Date:   Tue Sep 27 11:34:35 2016 +0800
    
        clean directories

---
+++ server.js
@@ -0,0 +1,160 @@
+const PATH = require('path')
+const mime = require('./lib/mime')
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const httpProxy = require('http-proxy')
+const Koa = require('koa')
+const ecstatic = require('ecstatic')
+
+const proxy = httpProxy.createProxyServer()
+const app = new Koa()
+
+const {configFilePath, config} = (x => {
+
+  var args = process.argv.slice(2)
+
+  var configFilePath = args[0] || './config'
+
+  if (/^[^\.\/]/.test(configFilePath)) {
+    configFilePath = './' + configFilePath
+  }
+
+  var config = require(configFilePath)
+
+  return {config, configFilePath}
+
+})()
+
+const host = config.host || '0.0.0.0'
+const port = config.port || 80
+
+const openFile = (path, flag) =>
+  new Promise((resolve, reject) => fs.open(path, flag, (err, fd) => err ? reject(err) : resolve(fd)))
+
+const readFile = (file, options) =>
+  new Promise((resolve, reject) => fs.readFile(file, options, (err, data) => err ? reject(err) : resolve(data)))
+
+const isFile = path =>
+  new Promise((resolve, reject) => fs.stat(path, (err, stats) => err ? reject(err) : resolve(stats.isFile())))
+
+const responseWithCharset = async (file, ctx) => {
+
+  var r = false
+
+  try {
+
+    var fd = await openFile(file, 'r')
+    var data = await readFile(fd)
+    var charset = data.toString(charset).indexOf('�') != -1 ? 'gbk' : 'utf-8'
+
+    ctx.type =  ctx.type + ';charset=' + charset
+    ctx.body = data
+
+    r = true
+
+  } catch (e) {
+  } finally {
+    if (fd) fs.close(fd)
+  }
+
+  return r
+}
+
+const responseAsStream = async (file, ctx) => {
+
+  var r = false
+
+  try {
+
+    if (await isFile(file)) {
+      var fd = await openFile(file, 'r')
+      ctx.body = fs.createReadStream(file, {fd})
+
+      r = true
+    }
+
+  } catch (e) {
+    if (fd) fs.close(fd)
+  }
+
+  return r
+}
+
+const proxyTo = (serverConfig, ctx) => {
+
+  var url = serverConfig.proxy_pass
+
+  ctx.respond = false
+
+  if (!url) {
+    return ecstatic({root: serverConfig.root})(ctx.req, ctx.res)
+  }
+
+  if (!/^http(s)?:\/\//.test(url)) {
+    url = ctx.protocol + '://' + url
+  }
+
+  proxy.web(ctx.req, ctx.res, {
+    target: url
+  })
+}
+
+app.use(async (ctx, next) => {
+  ctx.configFilePath = configFilePath
+  ctx.set('Access-Control-Allow-Origin', '*')
+  ctx.set('server', 'SohuTv-FE/node')
+  await next()
+})
+
+app.use(async (ctx, next) => {
+
+  var hostname = ctx.hostname
+  var serverConfig
+
+  config.servers.some(item => !!(serverConfig = item.name == hostname ? item : false))
+
+  if (!serverConfig) {
+    ctx.status = 404
+    return ctx.body = 'no server:' + hostname
+  }
+
+  if (ctx.method === 'POST') {
+    return proxyTo(serverConfig, ctx)
+  }
+
+  var path = ctx.path
+  if (typeof serverConfig.rewrite === 'function') {
+    path = serverConfig.rewrite(path)
+  }
+
+  var extname = PATH.extname(path).toLowerCase()
+
+  //ctx.type = mime[extname]
+  mime[extname] && ctx.set('Content-Type', mime[extname])
+
+  var filePath = PATH.join(serverConfig.root, path)
+  var originFilePath = PATH.join(serverConfig.root, ctx.path)
+
+  var files = filePath === originFilePath ? [filePath] : [filePath, originFilePath]
+
+  var respond = ['.js'].indexOf(extname) != -1 ? responseWithCharset : responseAsStream
+
+  var responsed = false
+  for (var file of files) {
+    if (responsed = await respond(file, ctx)) break
+  }
+
+  if (!responsed) {
+    return proxyTo(serverConfig, ctx)
+  }
+})
+
+http.createServer(app.callback()).listen(port, host)
+
+https.createServer({
+
+  key: fs.readFileSync(PATH.join(__dirname, './sslkey/a1.itc.cn_server.key')),
+  cert: fs.readFileSync(PATH.join(__dirname, './sslkey/a1.itc.cn.cer'))
+
+}, app.callback()).listen(443, host)
